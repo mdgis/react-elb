2f6dac2ec6eb644ac3adc2118af9d4d0431dc6f8
mdgis <mikdowd@gmail.com>
2019-11-10 11:01:57 -0500

    yes

---
+++ client/build/precache-manifest.5d8fd63443fdf92f18e1416548ea3dc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8de98ab365f1ed33eabef8ed1afc3f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98bcf1efdfa8d16da7fd",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c781cd145dcaec4c52b0",
+    "url": "/static/js/2.c7d6b653.chunk.js"
+  },
+  {
+    "revision": "98bcf1efdfa8d16da7fd",
+    "url": "/static/js/main.8b3ee15e.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
