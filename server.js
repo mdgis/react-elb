bb41311ef9c2b768442b9fad4879f1ed34fd9c17
mdgis <mikdowd@gmail.com>
2019-11-10 11:06:10 -0500

    yes

--- server.js
+++ server.js
@@ -9,14 +9,12 @@ app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.get('/', (req, res) => {
   res.send('just gonna send it');
 });
-
 app.get('/flower', (req, res) => {
   res.json({
     name: 'Dandelion',
     colour: 'Blue-ish'
   });
 });
-
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
 });
